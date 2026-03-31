b408124aed78727d5eeb706601079210962a86cc
RAHUL MAHTO <rahulmahto2486@gmail.com>
2026-04-01 00:10:32 +0530

    resume
    Replaced the MongoDB connection string with a placeholder for the API key.

--- resume-ats-analyzer-BackEnd/conn.js
+++ resume-ats-analyzer-BackEnd/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    'mongodb+srv://rahulmahto2486_db_user:AZ5hPJ1ZkhZXs1F7@cluster0.dkowy4c.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0'
+    'You Paste On DB API Key'
 )
 .then(() => {
     console.log("Database Connected Successfully!!");
